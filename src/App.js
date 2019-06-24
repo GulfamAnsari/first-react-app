be4effeb338b3a28db83aec435fa9cfaa3fe3c99
gulfamansari <gulfam.ansari@elucidata.io>
2019-06-24 16:05:52 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
-import  Header  from './components/Header';
+import Header from './components/Header';
 import { Footer } from './components/Footer';
+import { About } from './pages/about';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  const appName = "H";
+  const appName = "AppName";
   return (
     <div className="App">
-      <h1>{appName}</h1>
-      {/* header uses props */}
-      <Header title="header1" />
-      <Header title="header2" />
-      {/* Footer uses state */}
-      <Footer />
+      <Router>
+        <h1>{appName}</h1>
+        {/* header uses props */}
+        <Header title="header1" />
+        <Header title="header2" />
+        {/* Footer uses state */}
+        <Footer />
+        <Switch>
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
